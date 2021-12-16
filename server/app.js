ac5ddc768dae3e3191b1b38d74d19f061829bd5d
unknown <johanvaldemar.leoste@tptlive.ee>
2021-12-16 10:48:00 +0200

    small fixes

--- server/app.js
+++ server/app.js
@@ -23,7 +23,7 @@ app.use(express.static(path.join(__dirname, '../build')));
 app.use('/public', express.static(path.join(__dirname, '../public')));
 app.use('/api', router);
 app.get('*', (req, res) => {
-  res.sendFile('build/index.html', {root: path.join(__dirname, '../')});
+  res.sendFile('../build/index.html', {root: path.join(__dirname, '../')});
 });
 
 module.exports = app;
--- server/mongodb.js
+++ server/mongodb.js
@@ -3,7 +3,7 @@ const path = require('path')
 const fs = require('fs')
 //const MONGODB_URI: 'mongodb://root:root@mongo/if18?authSource=admin'
 //mongodb://localhost:27017
-const url = process.env.DATABASE_URL || 'mongodb://root:root@localhost:27017/?authSource=admin&directConnection=true&ssl=false'
+const url = process.env.DATABASE_URL || 'mongodb+srv://root:root@testapp.1s3bo.mongodb.net/test?retryWrites=true&w=majority'
 
 mongoose.connect(url, {
   useNewUrlParser: true,
